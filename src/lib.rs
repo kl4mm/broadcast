6725fa1c9105d467c084e6160a6e7f08d61e82b5
Subhan <s.ashraf9@protonmail.com>
2023-07-04 20:28:43 +0100

    multi node test pass

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 use std::{
-    collections::HashMap,
+    collections::{HashMap, HashSet},
     io::{self, Write},
 };
 
+use rand::Rng;
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize)]
@@ -56,7 +57,7 @@ pub fn init() -> io::Result<(String, Vec<String>)> {
         .to_vec()
         .iter()
         .map(|v| v.as_str().unwrap().to_owned())
-        .collect::<Vec<String>>();
+        .collect();
 
     init.body.msg_type = Type::InitOk;
 
@@ -80,19 +81,35 @@ pub fn reply(src: String, mut msg: Message) -> io::Result<()> {
     Ok(())
 }
 
+pub fn send(msg: Message) -> io::Result<()> {
+    let mut msg = serde_json::to_string(&msg).unwrap();
+    msg.push('\n');
+
+    let mut stdout = io::stdout();
+    stdout.write_all(msg.as_bytes())?;
+    stdout.flush()?;
+
+    Ok(())
+}
+
 #[derive(Default)]
 pub struct Node {
     id: String,
     node_ids: Vec<String>,
-    messages: Vec<u64>,
+    messages: HashSet<u64>,
     topology: HashMap<String, Vec<String>>,
+
+    // Eager probabalistic broadcast:
+    k: u8,
+    round: u8,
 }
 
 impl Node {
-    pub fn new(id: String, node_ids: Vec<String>) -> Self {
+    pub fn new(id: String, node_ids: Vec<String>, k: u8) -> Self {
         Self {
             id,
             node_ids,
+            k,
             ..Default::default()
         }
     }
@@ -104,16 +121,23 @@ impl Node {
                 let value = msg.body.fields["message"]
                     .as_u64()
                     .expect("Expected number value in broadcast message");
-                self.messages.push(value);
+                self.messages.insert(value);
 
                 msg.body.fields.clear();
                 reply(self.id.clone(), msg)?;
+
+                self.gossip(value)?;
             }
             Type::Read => {
                 msg.body.msg_type = Type::ReadOk;
-                msg.body
-                    .fields
-                    .insert("messages".into(), self.messages.clone().into());
+                msg.body.fields.insert(
+                    "messages".into(),
+                    self.messages
+                        .clone()
+                        .into_iter()
+                        .collect::<Vec<u64>>()
+                        .into(),
+                );
 
                 reply(self.id.clone(), msg)?;
             }
@@ -124,7 +148,9 @@ impl Node {
                 reply(self.id.clone(), msg)?;
             }
 
-            Type::Init | Type::InitOk | Type::BroadcastOk | Type::ReadOk | Type::TopologyOk => {
+            Type::BroadcastOk => {}
+
+            Type::Init | Type::InitOk | Type::ReadOk | Type::TopologyOk => {
                 unreachable!("Unexpected message type")
             }
         }
@@ -141,6 +167,52 @@ impl Node {
                 Ok(m) => self.handle_msg(m)?,
                 Err(e) => panic!("{e}"),
             }
+        }
+
+        Ok(())
+    }
+
+    // Eager probabalistic broadcast:
+    fn pick_targets(&self, k: u8) -> Vec<String> {
+        let mut targets = Vec::new();
+
+        for _ in 0..k {
+            let n = self.node_ids.len();
+
+            let mut rng = rand::thread_rng();
+            let i: usize = rng.gen_range(0..10) % n;
+
+            let mut candidate = self.node_ids[i % n].clone();
+            if candidate == self.id {
+                candidate = self.node_ids[(1 + i) % n].clone();
+            }
+
+            if !targets.contains(&candidate) {
+                targets.push(candidate);
+            }
+        }
+
+        targets
+    }
+
+    fn gossip(&self, value: u64) -> io::Result<()> {
+        for id in self.pick_targets(self.k) {
+            let message = Message {
+                src: self.id.clone(),
+                dest: id,
+                body: Body {
+                    msg_type: Type::Broadcast,
+                    id: None,
+                    in_reply_to: None,
+                    fields: [
+                        (String::from("message"), value.into()),
+                        // (String::from("round"), r.into()),
+                    ]
+                    .into(),
+                },
+            };
+
+            send(message)?;
         }
 
         Ok(())
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use broadcast::{init, Node};
 fn main() -> io::Result<()> {
     let (id, node_ids) = init()?;
 
-    let mut node = Node::new(id, node_ids);
+    let mut node = Node::new(id, node_ids, 3);
     node.run()?;
 
     Ok(())
